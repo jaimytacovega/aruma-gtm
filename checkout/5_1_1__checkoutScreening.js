c9a88254a778ba407076a5d0f46c21290904a988
Jaimy Taco <jaimy.taco.vega@gmail.com>
2026-06-02 20:09:32 -0500

    fix: 5.1.1

--- checkout/5_1_1__checkoutScreening.js
+++ checkout/5_1_1__checkoutScreening.js
@@ -1,18 +1,22 @@
 /**
- * Checkout profile step (#/profile): virtualPage. Paste before checkout-ui-custom.js.
+ * Checkout identification (#/profile, #/email): virtualPage. Paste before checkout-ui-custom.js.
  */
 ;((global) => {
-  const isCheckoutProfilePage = () =>
-    global.location.pathname.includes('/checkout') &&
-    global.location.hash.includes('/profile')
+  const isCheckoutScreeningPage = () => {
+    if (!global.location.pathname.includes('/checkout')) {
+      return false
+    }
+
+    const hash = global.location.hash
+
+    return hash.includes('/profile') || hash.includes('/email')
+  }
 
   global.create5_1_1__checkoutScreening = ({ pushToDataLayer }) => {
     let lastVirtualPageUrl = ''
 
-    const getPageTitle = () => document.title || 'Checkout'
-
     const pushCheckoutScreeningVirtualPage = () => {
-      if (!isCheckoutProfilePage()) {
+      if (!isCheckoutScreeningPage()) {
         return
       }
 
@@ -27,12 +31,12 @@
       pushToDataLayer({
         event: 'virtualPage',
         page_location: pageUrl,
-        page_title: getPageTitle(),
+        page_title: 'Aruma - Checkout - Identifcacion',
       })
     }
 
     const onHashChange = () => {
-      if (!isCheckoutProfilePage()) {
+      if (!isCheckoutScreeningPage()) {
         lastVirtualPageUrl = ''
         return
       }
